5d1676c5aa98880f8aa5aed25c27ee8f864d6b4f
Priya <Priya@LAPTOP-U1UV58Q9>
2020-02-22 10:33:48 +0530

    t

--- WebDriverDemo/src/testpack/DragandDrop.java
+++ WebDriverDemo/src/testpack/DragandDrop.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class DragandDrop {
-
+ 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Priya\\Desktop\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
