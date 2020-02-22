4d94d8d430cb8f77203264a19230ec5e1007f7bb
Priya <Priya@LAPTOP-U1UV58Q9>
2020-02-22 10:32:20 +0530

    test

--- WebDriverDemo/src/testpack/DragandDrop.java
+++ WebDriverDemo/src/testpack/DragandDrop.java
@@ -12,9 +12,12 @@ public class DragandDrop {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Priya\\Desktop\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://jqueryui.com/droppable/");
+		//Drop and drag
 		driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
 		WebElement drag = driver.findElement(By.id("draggable"));
 		WebElement drop = driver.findElement(By.id("droppable"));
+		//Actions is used for mouse actions in java
+
 		Actions act = new Actions(driver);
 		act.dragAndDrop(drag, drop).perform();
 		Thread.sleep(3000);
